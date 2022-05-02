7e86142de06f4c6cb7f7f3aae77983c527735dd9
MyeongSeok98 <myong2404@gmail.com>
2022-05-02 21:07:16 +0900

    초기 세팅

--- type.ts
+++ type.ts
@@ -1,4 +1,12 @@
 export type UserProps = {
     id:string,
-    password:string
+    password:string,
+    status:string
+}
+
+export type UserArrive = {
+    date:string,
+    time:string,
+    customer_id:string,
+    
 }
