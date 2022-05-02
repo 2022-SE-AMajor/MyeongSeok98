e65c6d2350b095ba2077bbf26cc9403361ab85e1
MyeongSeok98 <myong2404@gmail.com>
2022-05-02 20:59:09 +0900

    배포설정

--- app.ts
+++ app.ts
@@ -2,13 +2,17 @@ import express  from 'express';
 import cors from "cors";
 
 import { login } from './controller/auth';
+import { db } from './db/database';
 
 const app = express();
 
 app.use(express.json())
 app.use(cors());
 
-app.get('/login', login);
+app.post('/login', login);
+
+
+db.getConnection().then((connection)=>console.log('Mysql 연결완료'))
 
 app.listen(process.env.PORT||4000,()=>{
     console.log('4000번 포트에서 대기중');
